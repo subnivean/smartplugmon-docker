05b7beb5f99b6705ec77ca6e0475b8f61d84f52a
Mark Krautheim <mark.krautheim@gmail.com>
2022-11-12 15:10:02 +0000

    Now powered by Python!

---
+++ src/app.py
@@ -0,0 +1,33 @@
+import datetime
+import requests
+import sqlite3
+import sys
+
+# Read the plug IP addresses (e.g. '20 21') from the command line
+plugips = sys.argv[1:]
+
+CMDFMT = "http://192.168.1.{ip}/cm?cmnd=Status 8"
+
+ts = datetime.datetime.now().isoformat(sep=" ", timespec="seconds")
+powers = {"date": f'"{ts}"'}
+
+for plugip in plugips:
+    cmd = CMDFMT.format(ip=plugip)
+    try:
+        res = requests.post(cmd).json()
+    except requests.ConnectionError:
+        res = {"StatusSNS": {"ENERGY": {"Power": 0}}}
+
+    power = {f"sp{int(plugip):02d}": str(res["StatusSNS"]["ENERGY"]["Power"])}
+
+    powers = powers | power
+
+sql = (f"INSERT INTO spdata ({','.join(powers.keys())})"
+       f" VALUES ({','.join(powers.values())});")
+
+# Write to the sqlite database
+conn = sqlite3.connect("/data/smartplugmon.sqlite")
+cur = conn.cursor()
+cur.execute(sql)
+conn.commit()
+conn.close()
--- src/read_all.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Read all smart plugs as listed in the `for` loop below
-
-for i in 20 21 22 23 24 25
-do
-  ./read_cfsp.sh $i
-done
