893923f5360ef1d1aff48f859c360fa0ea5b548a
Mark Krautheim <mark.krautheim@gmail.com>
2022-11-23 02:33:32 +0000

    Update commands to use allinone image

--- run_app.sh
+++ run_app.sh
@@ -5,5 +5,5 @@ SCRIPT_PATH=$(dirname $(realpath -s $0))
 docker run --rm \
   -v $SCRIPT_PATH/data:/data \
   -v $SCRIPT_PATH/src:/app \
-  smartplugmon python app.py "$@"
+  allinone-py311 python app.py "$@"
 
